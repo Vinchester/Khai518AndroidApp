a3b9a8174f64726318d4a78e1b46a0b71a3b140a
DIT Happy <m.h.kuznetsov@student.khai.edu>
2023-04-28 22:11:56 +0300

    Added: Tasks on the main screen to appear by clicking on one of the buttons at selection.xml

--- app/src/main/java/com/example/khaiapp/MainActivity.java
+++ app/src/main/java/com/example/khaiapp/MainActivity.java
@@ -3,11 +3,16 @@ package com.example.khaiapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
 import java.util.ArrayList;
+import java.util.List;
+
+import com.example.khaiapp.dataHandling.dataHandling;
 
 
 /*
@@ -38,7 +43,7 @@ public class MainActivity extends AppCompatActivity {
     *
     * @ author: Vinchester
     * @ version: 1.0
-    * @ from: 27/032023
+    * @ from: 27/03/2023
     *
     * */
     public void setSelectionView(View view) {
@@ -59,6 +64,12 @@ public class MainActivity extends AppCompatActivity {
             String buttonText = subjects.get(i);
             button.setText(buttonText);
             buttonsContainer.addView(button);
+            button.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    setMainView(buttonText);
+                }
+            });
         }
 
     }
@@ -67,9 +78,79 @@ public class MainActivity extends AppCompatActivity {
     *
     * Changing current layout to activity_main.xml
     *
+    * @ author: Vinchester
+    * @ version: 1.0
+    * @ from: 28/04/2023
     * */
 
-    public void setMainView(View view){
+    public void setMainView(String subjectName){
         setContentView(R.layout.activity_main);
+
+        List<List<String>> tasks = dataHandling.testGetSubjectTasks(subjectName);
+
+        /*
+        * Loop, where we add tasks onto activity_main.xml
+        * */
+
+        for (int i = 0; i < tasks.size();i++){
+            LinearLayout tasksContainer = findViewById(R.id.tasks_container);
+            RelativeLayout relativeLayout = new RelativeLayout(this);
+            RelativeLayout.LayoutParams layoutParams = new RelativeLayout.LayoutParams(
+                    RelativeLayout.LayoutParams.MATCH_PARENT,
+                    RelativeLayout.LayoutParams.WRAP_CONTENT
+            );
+            layoutParams.setMargins(20, 20, 20, 20);
+            relativeLayout.setLayoutParams(layoutParams);
+            relativeLayout.setBackgroundColor(0xFFBCBCBC);
+
+            TextView taskNameTextView = new TextView(this);
+            RelativeLayout.LayoutParams taskNameTextViewParams = new RelativeLayout.LayoutParams(
+                    RelativeLayout.LayoutParams.WRAP_CONTENT,
+                    RelativeLayout.LayoutParams.WRAP_CONTENT
+            );
+            taskNameTextViewParams.addRule(RelativeLayout.ALIGN_PARENT_TOP);
+            taskNameTextViewParams.addRule(RelativeLayout.ALIGN_PARENT_LEFT);
+            taskNameTextView.setLayoutParams(taskNameTextViewParams);
+            taskNameTextView.setText(tasks.get(i).get(0));
+            taskNameTextView.setTextColor(0xFF2B2B2B);
+            taskNameTextView.setTextSize(35);
+            taskNameTextView.setPaddingRelative(0, 20, 0, 0);
+            taskNameTextView.setId(View.generateViewId());
+
+            TextView taskThemeTextView = new TextView(this);
+            RelativeLayout.LayoutParams taskThemeTextViewParams = new RelativeLayout.LayoutParams(
+                    RelativeLayout.LayoutParams.WRAP_CONTENT,
+                    RelativeLayout.LayoutParams.WRAP_CONTENT
+            );
+            taskThemeTextViewParams.addRule(RelativeLayout.BELOW, taskNameTextView.getId());
+            taskThemeTextViewParams.addRule(RelativeLayout.ALIGN_PARENT_LEFT);
+            taskThemeTextView.setLayoutParams(taskThemeTextViewParams);
+            taskThemeTextView.setText(String.format("Theme: %s", tasks.get(i).get(1)));
+            taskThemeTextView.setTextColor(0xFF2B2B2B);
+            taskThemeTextView.setTextSize(30);
+            taskThemeTextView.setPaddingRelative(0, 0, 0, 0);
+            taskThemeTextView.setId(View.generateViewId());
+
+            TextView linkTextView = new TextView(this);
+            RelativeLayout.LayoutParams linkTextViewParams = new RelativeLayout.LayoutParams(
+                    RelativeLayout.LayoutParams.WRAP_CONTENT,
+                    RelativeLayout.LayoutParams.WRAP_CONTENT
+            );
+            linkTextViewParams.addRule(RelativeLayout.BELOW, taskThemeTextView.getId());
+            linkTextViewParams.addRule(RelativeLayout.ALIGN_PARENT_LEFT);
+            linkTextView.setLayoutParams(linkTextViewParams);
+            linkTextView.setText(String.format("Link: %s", tasks.get(i).get(2)));
+            linkTextView.setTextColor(0xFF2B2B2B);
+            linkTextView.setTextSize(30);
+            linkTextView.setPaddingRelative(0, 0, 0, 0);
+
+            relativeLayout.addView(taskNameTextView);
+            relativeLayout.addView(taskThemeTextView);
+            relativeLayout.addView(linkTextView);
+
+            tasksContainer.addView(relativeLayout);
+        }
+
+
     }
 }
--- app/src/main/java/com/example/khaiapp/dataHandling/dataHandling.java
+++ app/src/main/java/com/example/khaiapp/dataHandling/dataHandling.java
@@ -8,6 +8,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 
 import org.springframework.web.client.RestTemplate;
 
+import java.util.ArrayList;
 import java.util.List;
 
 
@@ -35,6 +36,57 @@ public class dataHandling {
         List<String> subjects = objectMapper.readValue(response, new TypeReference<List<String>>(){});
         Log.d("SubjectsTest", String.valueOf(subjects));
         return subjects;
+    }
+
+    /*
+    * Getting List of tasks(type: List<String>) from server by giving subject name as argument
+    *
+    * @ currently: unused
+    * @ from 28/04/2023
+    */
+
+    public static List<List<String>> getSubjectTasks(String subject) throws JsonProcessingException {
+        RestTemplate restTemplate = new RestTemplate();
+        String url = String.format("http://localhost:8081/subjects/getTasks?subject=%s", subject);
+        String response = restTemplate.getForObject(url, String.class);
+        ObjectMapper objectMapper = new ObjectMapper();
+        List<List<String>> tasks = objectMapper.readValue(response, (TypeReference<List<List<String>>>) new TypeReference<List<List<String>>>() {});
+        Log.d("SubjectsTest", String.valueOf(tasks));
+        return tasks;
+
+    }
+
+    /*
+    * Giving same thing as getSubjectTasks would give
+    *
+    * @ currently: used
+    * @ from 28/04/2023
+    */
+
+
+    public static List<List<String>> testGetSubjectTasks(String name){
+        List<List<String>> tasks = new ArrayList<>();
+
+        ArrayList<String> testTask1 = new ArrayList<>();
+        testTask1.add(name);
+        testTask1.add("Matrix");
+        testTask1.add("http://518theBest");
+
+        ArrayList<String> testTask2 = new ArrayList<>();
+        testTask2.add(name);
+        testTask2.add("Binary Tree");
+        testTask2.add("http://518theBest");
+
+        ArrayList<String> testTask3 = new ArrayList<>();
+        testTask3.add(name);
+        testTask3.add("Sorting");
+        testTask3.add("http://518theBest");
+
+        tasks.add(testTask1);
+        tasks.add(testTask2);
+        tasks.add(testTask3);
+
+        return tasks;
     }
 
 }
